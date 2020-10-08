476cd29cc04f319e72210c8bb2902889269fd3b0
ledumaelle <maelle.lolitadu22@gmail.com>
2020-10-08 17:12:39 +0200

    Réactiver/Désactiver User + Supp (pas encore de confirmation)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,5 +31,73 @@ class AdminController extends AbstractController
         return $this->render('admin/listUtilisateurs.html.twig', [
             'participants' => $participants
         ]);
+    }
+
+    /**
+     * @Route("/utilisateur/{id}/desactiver", name="app_admin_utilisateur_desactiver", requirements={"id": "\d+"})
+     * @param $id
+     * @param ParticipantRepository $participantRepository
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function desactiverUtilisateur($id, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager)
+    {
+        $participant = $participantRepository->find($id);
+
+        if(empty($participant)) {
+            throw $this->createNotFoundException("Participant non trouvé");
+        }
+
+        $participant->setActif(false);
+        $entityManager->persist($participant);
+        $entityManager->flush();
+
+        $this->addFlash("success","L'utilisateur a bien été désactivé temporairement ! ");
+        return $this->redirectToRoute("app_admin_utilisateurs");
+    }
+
+    /**
+     * @Route("/utilisateur/{id}/reactiver", name="app_admin_utilisateur_reactiver", requirements={"id": "\d+"})
+     * @param $id
+     * @param ParticipantRepository $participantRepository
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function reactiverUtilisateur($id, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager)
+    {
+        $participant = $participantRepository->find($id);
+
+        if(empty($participant)) {
+            throw $this->createNotFoundException("Participant non trouvé");
+        }
+
+        $participant->setActif(true);
+        $entityManager->persist($participant);
+        $entityManager->flush();
+
+        $this->addFlash("success","L'utilisateur a bien été réactivé ! ");
+        return $this->redirectToRoute("app_admin_utilisateurs");
+    }
+
+    /**
+     * @Route("/utilisateur/{id}/delete", name="app_admin_utilisateur_delete", requirements={"id": "\d+"})
+     * @param $id
+     * @param ParticipantRepository $participantRepository
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function delete($id, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager)
+    {
+        $participant = $participantRepository->find($id);
+
+        if(empty($participant)) {
+            throw $this->createNotFoundException("Participant non trouvé");
+        }
+
+        $entityManager->remove($participant);
+        $entityManager->flush();
+
+        $this->addFlash("success","L'utilisateur a bien été supprimé ! ");
+        return $this->redirectToRoute("app_admin_utilisateurs");
     }
 }
