e45f8f8e48720d42c5026f62f1068a3b23cbe669
Julien <julien.vandamme2019@campus-eni.fr>
2020-10-12 09:28:13 +0200

    ajout inscription et desistement

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 
 
 use App\Entity\Etat;
+use App\Entity\Inscription;
 use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\SortieType;
+use DateTime;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 class SortieController extends AbstractController
 {
@@ -132,6 +135,120 @@ class SortieController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     *
+     * @Route("/sortie/inscription/{id}",
+     *     name="inscrit_sortie",
+     *     requirements={"id": "\d+"})
+     * @param Request $request
+     * @param Sortie $sortie
+     * @return Response
+     *
+     */
+    public function inscriptionSortie(Request $request, Sortie $sortie, LoggerInterface $logger)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $userName = $this->getUser()->getUsername();
+
+        $user = $em->getRepository(Participant::class)->findOneByMail($userName);
+
+
+        foreach($sortie->getInscriptions() as $inscription){
+
+            if($inscription->getParticipant()==$user) {
+
+
+                return $this->render('sortie/confirmation.html.twig', [
+                    'message' => 'vous etes deja inscrit pour cette sortie',
+                    'class'=>'alert alert-warning'
+                ]);
+            }
+
+        }
+
+
+        $newInscription = new Inscription();
+
+
+        $newInscription->setDateCreated();
+        $newInscription->setDateInscription(new DateTime());
+        $newInscription->setParticipant($user);
+
+        $sortie->addInscription($newInscription);
+
+        $em->persist($sortie);
+        $em->persist($newInscription);
+        $em->flush();
+
+        return $this->render('sortie/confirmation.html.twig',[
+            'message'=>'votre inscription a bien etait prise en compte',
+            'class'=>'alert alert-success'
+        ]);
+
+
+
+
+
+
+
+    }
+
+
+    /**
+     *
+     * @Route("/sortie/desiste/{id}",
+     *     name="desiste_sortie",
+     *     requirements={"id": "\d+"})
+     * @param Request $request
+     * @param Sortie $sortie
+     * @return Response
+     *
+     */
+    public function desisteSortie(Request $request, Sortie $sortie, LoggerInterface $logger)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $userName = $this->getUser()->getUsername();
+
+        $user = $em->getRepository(Participant::class)->findOneByMail($userName);
+
+
+        foreach($sortie->getInscriptions() as $inscription){
+
+            if($inscription->getParticipant()==$user) {
+
+
+                
+
+                $sortie->removeInscription($inscription);
+
+                $em->persist($sortie);
+                $em->persist($inscription);
+                $em->flush();
+
+                return $this->render('sortie/confirmation.html.twig',[
+                    'message'=>'votre désistement a bien etait prise en compte',
+                    'class'=>'alert alert-success'
+                ]);
+               
+            }
+
+        }
+
+        return $this->render('sortie/confirmation.html.twig', [
+            'message' => 'vous n\'etes pas inscrit pour cette sortie',
+            'class'=>'alert alert-warning'
+        ]);
+
+
+        
+
+    }
+
+
+
 
 }
 
