d7d56769c2681beb68d143b4debfcc095aa48323
Angelique Le Roux <angelique.leroux_stg@cooperl.com>
2020-10-14 16:36:57 +0200

    début visuel uniforme sauf form sortie

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -19,82 +19,77 @@ class ParticipantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('imageFile',VichImageType::class,
-                [
-                    'label' => false,
+        $builder->add('imageFile', VichImageType::class, [
+            'label' => false,
+            'required' => false,
+            'allow_delete' => false,
+            'delete_label' => false,
+            'download_uri' => false,
+            'download_label' => false,
+        ])
+                ->add('pseudo', TextType::class, [
+                    'attr' => [
+                        'class' => 'form-control mb-4',
+                    ],
+                    'label' => 'Pseudo',
+                ])
+                ->add('mail', TextType::class, [
+                    'attr' => [
+                        'class' => 'form-control mb-4',
+                    ],
+                    'label' => 'Mail',
+                ])
+                ->add('nom', TextType::class, [
+                    'attr' => [
+                        'class' => 'form-control mb-4',
+                    ],
+                    'label' => 'Nom',
+                ])
+                ->add('prenom', TextType::class, [
+                    'attr' => [
+                        'class' => 'form-control mb-4',
+                    ],
+                    'label' => 'Prénom',
+                ])
+                ->add('telephone', TextType::class, [
+                    'attr' => [
+                        'class' => 'form-control mb-4',
+                    ],
+                    'label' => 'Téléphone',
+                ])
+                ->add('motPasse', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Les mot de passe doivent être identiques.',
                     'required' => false,
-                    'allow_delete' => false,
-                    'delete_label' => false,
-                    'download_uri' => false,
-                    'download_label' => false
-            ])
-            ->add('pseudo', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control mb-4',
-                ],
-                'label' => 'Pseudo'
-            ])
-            ->add('mail', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control mb-4'
-                ],
-                'label' => 'Mail'
-            ])
-            ->add('nom',TextType::class, [
-                'attr' => [
-                    'class' => 'form-control mb-4'
-                ],
-                'label' => 'Nom'
-            ])
-            ->add('prenom',TextType::class, [
-                'attr' => [
-                    'class' => 'form-control mb-4'
-                ],
-                'label' => 'Prénom'
-            ])
-            ->add('telephone', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control mb-4'
-                ],
-                'label' => 'Téléphone'
-            ])
-            ->add('motPasse', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les mot de passe doivent être identiques.',
-                'required' => false,
-                'first_options'  => [
-                    'label' => false,
-                    'attr' => [
-                        'class' => 'form-control mb-4',
-                        'placeholder' => 'Mot de passe'
+                    'first_options' => [
+                        'label' => 'Mot de passe',
+                        'attr' => [
+                            'class' => 'form-control mb-4',
+                        ],
+                        'empty_data' => ' ',
                     ],
-                    'empty_data' => ' ',
-                ],
-                'second_options' => [
-                    'label' => false,
-                    'attr' => [
-                        'class' => 'form-control mb-4',
-                        'placeholder' => 'Confirmer le mot de passe'
+                    'second_options' => [
+                        'label' => 'Confirmer le mot de passe',
+                        'attr' => [
+                            'class' => 'form-control mb-4',
+                        ],
+                        'empty_data' => ' ',
                     ],
-                    'empty_data' => ' ',
-                ],
-            ])
-            ->add('campus',EntityType::class,[
-                'class' => Campus::class,
-                'choice_label' => 'nom',
-                'placeholder' => 'Sélectionnez un campus',
-                'required' => true,
-                'label' => 'Campus',
-                'attr' => [
-                    'class' => 'mdb-select md-form'
-                ],
-                'query_builder' => function(EntityRepository $er){
-                    return $er->createQueryBuilder('c')
-                        ->orderBy('c.nom',"ASC");
-                }
-            ])
-        ;
+                ])
+                ->add('campus', EntityType::class, [
+                    'class' => Campus::class,
+                    'choice_label' => 'nom',
+                    'placeholder' => 'Sélectionnez un campus',
+                    'required' => true,
+                    'label' => 'Campus',
+                    'attr' => [
+                        'class' => 'mdb-select md-form',
+                    ],
+                    'query_builder' => function(EntityRepository $er) {
+                        return $er->createQueryBuilder('c')
+                                  ->orderBy('c.nom', "ASC");
+                    },
+                ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
